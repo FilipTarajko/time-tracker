d06efc114c06811f8e3d6f4b45af6d7104016b30
Filip Tarajko <76916685+FilipTarajko@users.noreply.github.com>
2025-04-17 22:24:23 +0200

    Add "(no description)" to entry display

--- src/components/EntryDescription.vue
+++ src/components/EntryDescription.vue
@@ -1,21 +1,50 @@
 <template>
-  <div class="description" contenteditable @blur="updateDescription">
-    {{ entry.description }}
+  <div
+    class="description"
+    ref="descriptionContenteditable"
+    contenteditable
+    @focus="onFocus"
+    @blur="onBlur"
+    :style="`${(entry.description || isFocused) ? '' : 'opacity: 0.3; font-style: italic;'}`"
+  >
+    {{ entry.description || FALLBACK_TEXT }}
   </div>
 </template>
 
 <script setup lang="ts">
 import { Entry, useEntriesStore } from 'stores/entriesStore';
+import { ref } from 'vue';
+
+const FALLBACK_TEXT = '(no description)';
+
+const entriesStore = useEntriesStore();
 
 const props = defineProps<{
   entry: Entry;
 }>();
 
-const entriesStore = useEntriesStore();
+const descriptionContenteditable = ref<HTMLElement | null>(null);
 
-function updateDescription(e: Event) {
+let isFocused = ref(false);
+
+function onFocus(e: Event) {
+  isFocused.value = true;
+  if (!props.entry.description) {
+    const target = e.target as HTMLElement;
+    target.innerText = '';
+  }
+}
+
+function onBlur(e: Event) {
+  isFocused.value = false;
   const target = e.target as HTMLElement;
+  if (target.innerHTML == '<br>' || target.innerHTML == '&nbsp;') {
+    target.innerHTML = '';
+  }
   entriesStore.updateDescriptionOfEntry(props.entry, target.innerText);
+  if (target.innerText == '') {
+    target.innerText = FALLBACK_TEXT;
+  }
 }
 </script>
 
